d561fed91679a97c0f608bedab2cdeffb0433e11
kmchord9 <kmchord9@gmial.com>
2020-05-24 14:32:35 +0900

    add bundle module

--- src/vue/webpack.config.js
+++ src/vue/webpack.config.js
@@ -1,11 +1,32 @@
 const path = require('path')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist/static')
+    path: path.resolve(__dirname, 'dist/static'),
+    filename: 'bundle.js'
+  },
+  module:{
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader'
+      }
+    ]
   },
   resolve: {
-    extensions: ['.webpack.js', '.web.js', '.js']
-  }
+    extensions: ['.webpack.js', '.web.js', '.js', 'vue'],
+    alias: {
+      vue$: 'vue/dist/vue.esm.js',
+    }
+  },
+  plugins: [
+    new VueLoaderPlugin()
+  ]
 }
